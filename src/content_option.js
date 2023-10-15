64dded8725ce437e1731f90acbb7ffbf7acffda3
Divyanshu81 <divyanshu52singhal@gmail.com>
2023-10-15 18:46:47 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,110 @@
+const logotext = "Divyanshu";
+const meta = {
+    title: "Divyanshu Kumar Singhal",
+    description: "I’m Divyanshu Kumar Singhal Full stack devloper,Compitetive coder",
+};
+
+const introdata = {
+    title: "I’m Divyanshu Kumar Singhal",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",  
+    },
+    description: "Currently I am a 3rd year Student in VIT Bhopal pursing my Btech degree in CSE",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "● Python Developer (developed GUI apps,web apps,intermediate knowledge of data visualization).2+ years of experience in Competitive Programming (4 STAR rating in Codechef and pupil in Codeforces) Solved over 450+ Coding Problems on different platforms over Leetcode, HackerRank, Newton School, Google Kickstart, etc.  Skilled badge holder of GoogleCloud Facilitator Programs (Student) Excellent logical and analytical reasoning, strong communication skills. Strong inclination towards programming, algorithm building. Enjoy engineering solutions to challenging problems.Passion to learn and work on new challenges, believe in self-learning.",
+};
+const worktimeline = [{
+        jobtitle: "Student",
+        where: "VIT Bhopal",
+        date: "2025",
+    },
+    {
+        jobtitle: "Intern",
+        where: "Ethnus",
+        date: "2023",
+    }
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Can Design Cool Web interface if required.",
+    },
+    {
+        title: "Model training",
+        description: "I can train a model for a given data can make some useful insight from it.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://www.computersciencedegreehub.com/wp-content/uploads/2021/10/algorithm-specialist.jpg",
+        description: "Algorithmic Calculator.",
+        link: "https://divyanshu81.github.io/Algorithmic-calculators/",
+    },
+    {
+        img: "https://miro.medium.com/v2/resize:fit:828/format:webp/1*lJ32Bl-lHWmNMUSiSq17gQ.png",
+        description: "Html and css work.",
+        link: "https://divyanshu81.github.io/MernIntershipTasks/",
+    },
+    {
+        img: "https://www.sessions.edu/wp-content/uploads/web_program_update.jpg",
+        description: "Bootstrap and JS work.",
+        link: "https://divyanshu81.github.io/BS_JS_Tasks/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "divyanshu52singhal@gmail.com",
+    YOUR_FONE: "+91 7983475934",
+    description: "Thank YOU",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Divyanshu81",
+    linkedin: "https://www.linkedin.com/in/divyanshu-kumar-singhal-551b08212/",
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
